6809daca42a170f1fe82079ca477756f3b3566d5
zyad27 <zyadt782@gmail.com>
2024-12-10 14:50:13 +0200

    reverse arr function

---
+++ Session10Task/script.js
@@ -0,0 +1,15 @@
+arr = [1, 2, 3, 4, 5]
+arr1 = ["a", "b", "c", "d", "e"]
+
+// Function that reverses an array
+function arrReverse(input) {
+    for (let i = (input.length-1) ; i >= 0 ; i--){
+        console.log(input[i])
+    }
+}
+
+arrReverse(arr)
+arrReverse(arr1)
+
+/*---------------------------------------------- */
+
