7b8a31ea6049fea761b971adfe2938859d7ad0a3
zyad27 <zyadt782@gmail.com>
2024-12-10 16:01:04 +0200

    javascriptTask

--- Session10Task/script.js
+++ Session10Task/script.js
@@ -13,3 +13,26 @@ arrReverse(arr1)
 
 /*---------------------------------------------- */
 
+function Divide(input) {
+    if (!(input%10)) return console.log("the number is divisible")
+    return console.log("the number isn't divisible")
+}
+
+Divide(13) //isn't divisible
+Divide(20) // is divisible
+
+/*---------------------------------------------- */
+arr2 = [50, 87 ,5, 120, 12]
+
+// function that retrives the biggest number from an array
+function maxNumber(input) {
+    function compNumber(a, b) {
+        if( a > b ) return 1
+        if( a == b ) return 0
+        if( a < b ) return -1
+    }
+    input = input.sort(compNumber)
+    console.log(input[(input.length-1)])
+}
+
+maxNumber(arr2)
